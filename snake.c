42d3d5c9acac1c05cf2a7af833a57fcba2b4aa73
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-01-29 23:57:06 -0500

    wrote pseudocode

--- snake.c
+++ snake.c
@@ -1,51 +1,59 @@
 #include <stdio.h>
-#include <time.h>
-#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
-struct Screen {
-	int width;
-	int height;
-	int frames_per_second;
+int SCREEN_WIDTH = 30;
+int SCREEN_HEIGHT = 30;
+
+struct Coordinate {
+	int x;
+	int y;
 };
 
-struct Screen screen = {
-	20,
-	20,
-	1,
+int SNAKE_STARTING_LENGTH = 3;
+
+struct Coordinate snake_pixels[SNAKE_STARTING_LENGTH];
+struct Coordinate snake_velocity;
+struct Coordinate snake_head_position;
+
+
+void initialize_snake_pixels(void) {
+	struct Coordinate snake_velocity = {.x=0, .y=1};
+
+	struct Coordinate snake_head_position = {floor(SCREEN_WIDTH/2), floor(SCREEN_HEIGHT/3)};
+	struct Coordinate starting_tail_fragment_2 = {.x=snake_head_position.x, .y=(snake_head_position.y - 1)};
+	struct Coordinate starting_tail_fragment_3 = {.x = starting_tail_fragment_2.x, .y = (starting_tail_fragment_2.y - 1)};
+	
+	// APPEND THE THREE TO snake_pixels
 };
 
-void draw_row() {
-	for (int i=0; i<screen.width; i++) {
-		printf(".");
-	}
-	
-	printf("\n");
-}
+// calculate_snake_pixels()
 
-void draw_screen() {
-	for (int i=0; i<screen.height; i++) {
-		draw_row();
-	}
-}
-
-snake_start_position() {
-	
-}
-
-void initialise_snake() {
-
-}
-
-// const char* draw_screen() {
-// 	for (int i=0; i<20; i++) {
-
-// 	}
-// }
+// char BACKGROUND = ".";
 
 int main(void) {
+	initialize_snake_pixels();
 
-	draw_screen();
-	initialise_snake();
+	bool running = true;
 
-	return(0);
+	
+
+	// 	while(running) {
+		
+	// 		Get_user_input()
+
+	// 		recalculate_snake_pixels();
+
+	// 		If (snake_will_hit_wall() || snake_will_eat_itself) {
+	// 			break;
+	// 		}
+
+	// 		If snake_ate_fruit {
+	// 			recalculate_fruit_pixels();
+	// 		}
+
+	// 		draw_screen(fruit_pixels, snake_pixels);
+	// 	}
+
+	return 0;
 }
