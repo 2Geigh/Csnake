e470c61b45c551c69cb31094ba2da41253753f1f
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-02-04 01:54:36 -0500

    fixed initizialize_snake_pixels scope
    Turns out it was actually redefining the global constants within the function, and therefore only working in the function's scope.
    
    It was as simple as changing the assignment statements to be reassignments by removing the type indicators.

--- snake.c
+++ snake.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <stdbool.h>
 
-int SCREEN_WIDTH = 30;
-int SCREEN_HEIGHT = 30;
+#define SCREEN_WIDTH 30
+#define SCREEN_HEIGHT 30
 
 struct Coordinate {
 	int x;
@@ -18,24 +18,37 @@ struct Coordinate snake_head_position;
 
 
 void initialize_snake_pixels(void) {
-	struct Coordinate snake_velocity = {.x=0, .y=1};
+	snake_velocity = (struct Coordinate){.x=0, .y=1};
 
-	struct Coordinate snake_head_position = {floor(SCREEN_WIDTH/2), floor(SCREEN_HEIGHT/3)};
-	struct Coordinate starting_tail_fragment_2 = {.x=snake_head_position.x, .y=(snake_head_position.y - 1)};
-	struct Coordinate starting_tail_fragment_3 = {.x = starting_tail_fragment_2.x, .y = (starting_tail_fragment_2.y - 1)};
+	snake_head_position = (struct Coordinate){floor(SCREEN_WIDTH/2), floor(SCREEN_HEIGHT/3)};
+	struct Coordinate snake_tail_fragment_2 = {.x=snake_head_position.x, .y=(snake_head_position.y - 1)};
+	struct Coordinate snake_tail_fragment_3 = {.x = snake_tail_fragment_2.x, .y = (snake_tail_fragment_2.y - 1)};
 	
+	snake_pixels[0] = snake_head_position;
+	snake_pixels[1] = snake_tail_fragment_2;
+	snake_pixels[2] = snake_tail_fragment_3;
 	// APPEND THE THREE TO snake_pixels
 };
+
+void draw_screen(void);
+	// This function will take in arrays of type Coordinate,
+	// map the points onto a grid of dimensions SCREEN_WIDTH and SCREEN_HEIGHT,
+	// and finally draw out the grid row by row
+
+	//  
 
 // calculate_snake_pixels()
 
 // char BACKGROUND = ".";
 
 int main(void) {
-	initialize_snake_pixels();
-
 	bool running = true;
 
+	initialize_snake_pixels();
+
+	for (int i = 0; i < sizeof(snake_pixels) / sizeof(snake_pixels[0]); i++) {
+		printf("(%d,%d)\n", snake_pixels[i].x, snake_pixels[i].y);	
+	}
 	
 
 	// 	while(running) {
